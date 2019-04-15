82dd789b8acafef6dcd796d4d39197ba8c16299e
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-04-15 03:20:14 -0600

    Adds character set test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -57,5 +57,13 @@ class EnigmaTest < Minitest::Test
   end
 
 
+  def test_character_set
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                " "]
+    assert_equal expected, @enigma.character_set
+  end
+
+
 
 end
