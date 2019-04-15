8247dfa21caf93778900e5bce23a9a84e6a9a6be
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-04-15 13:55:22 -0600

    Adds test for encrypt method

---
+++ test/encrypt_test.rb
@@ -0,0 +1,50 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/shiftgenerator'
+require './lib/enigma'
+require './lib/encrypt'
+
+class EncryptTest < Minitest::Test
+
+  def setup
+    @encrypt = Encrypt.new("38492", "0943", "Hello")
+  end
+
+  def test_encrypt_exists
+    assert_instance_of Encrypt, @encrypt
+  end
+
+  def test_split_characters_works_creates_array_of_split_characters
+
+    assert_equal ["m","i","l","l","s"], @encrypt.split_characters("Mills")
+  end
+
+  def test_validate_characters_returns_true_or_false
+    assert_equal true, @encrypt.validate_characters("Mills")
+    assert_equal false, @encrypt.validate_characters("Mills!")
+  end
+
+  def test_it_returns_array_of_chars_and_index_numbers
+     skip
+     assert_equal [["m", 0], ["i", 1], ["l", 2], ["l", 3], ["s", 4]], @encrypt.chars_and_index("Mills")
+  end
+
+
+  def test_entered_keys_and_offsets_can_create_shift
+    expected = {:a=>38,
+                :b=>93,
+                :c=>53,
+                :d=>95}
+    assert_equal expected, @encrypt.enter_shift("38492", "0943")
+  end
+
+  def test_shift_alphabet_creates_new_alphabet
+    assert_equal ["j"], @encrypt.shift_alphabet("m", 9)
+  end
+
+  def test_it_can_rotate_the_characters
+    assert_equal "lm ol", @encrypt.encrypt("38492", "0943", "Hello")
+  end
+
+end
