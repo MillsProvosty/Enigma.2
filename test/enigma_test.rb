28efb3c198f1ede280a5b652859e9dc4b33bbab2
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-04-15 03:09:46 -0600

    Adds generate date test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,3 +1,9 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/shiftgenerator'
+require './lib/enigma'
+
 class EnigmaTest < Minitest::Test
   def setup
     @enigma = Enigma.new
@@ -5,6 +11,10 @@ class EnigmaTest < Minitest::Test
 
   def test_enigma_exists
     assert_instance_of Enigma, @enigma
+  end
+
+  def test_it_can_generate_todays_date
+    assert_equal "130419", @enigma.generate_todays_date
   end
 
 end
