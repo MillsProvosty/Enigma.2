3e8f1ba9095924e9507879138a549451f95d39e2
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-04-15 03:20:37 -0600

    Adds character set method

--- lib/shiftgenerator.rb
+++ lib/shiftgenerator.rb
@@ -43,5 +43,7 @@ module ShiftGenerator
     end
   end
 
-
+  def character_set
+   characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+  end
 end
