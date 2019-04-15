7347a07d3aad83d84631f3c45f734225027b573f
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-04-15 03:11:45 -0600

    Adds square the date method

--- lib/shiftgenerator.rb
+++ lib/shiftgenerator.rb
@@ -2,6 +2,10 @@ module ShiftGenerator
 
   def generate_todays_date
      Time.now.strftime("%d%m%y")
-   end
+  end
+
+  def square_the_date
+   (generate_todays_date.to_i * generate_todays_date.to_i).to_s
+  end
 
 end
