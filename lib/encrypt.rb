3a5090b6029da871961a96ea3150f5dd621ea57b
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-04-15 13:55:41 -0600

    Adds functioning method for encrypt

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,4 +1,18 @@
-module Encrypt
+require_relative './shiftgenerator'
+
+class Encrypt
+  include ShiftGenerator
+  attr_reader :key,
+              :offset,
+              :message,
+              :entered_shift_hash
+  def initialize(key, offset, message)
+    @key = key
+    @offset = offset
+    @message = message
+    @entered_shift_hash = Hash.new(0)
+  end
+
   def split_characters(message)
     split_message = message.downcase.split(//)
     split_message
@@ -19,42 +33,51 @@ module Encrypt
     letter_and_index
   end
 
-
   def enter_shift(key, offset)
-    entered_shift_hash = {}
-    entered_shift_hash[:a] = (key[-5..-4].to_i + offset[0].to_i)
-    entered_shift_hash[:b] = (key[-4..-3].to_i + offset[1].to_i)
-    entered_shift_hash[:c] = (key[-3..-2].to_i + offset[2].to_i)
-    entered_shift_hash[:d] = (key[-2..-1].to_i + offset[3].to_i)
-    entered_shift_hash
+    @entered_shift_hash[:a] = (key[-5..-4].to_i + offset[0].to_i)
+    @entered_shift_hash[:b] = (key[-4..-3].to_i + offset[1].to_i)
+    @entered_shift_hash[:c] = (key[-3..-2].to_i + offset[2].to_i)
+    @entered_shift_hash[:d] = (key[-2..-1].to_i + offset[3].to_i)
+    @entered_shift_hash
   end
 
-  def encrypt(key, offset, message)
-  split_message = split_characters(message)
-  split_message.each do |char|
-    if !validate_characters(char)
-      "Invalid Entry, please only use letters and spaces."
-    else
-      letter_and_index = chars_and_index(char)
-      letter_and_index.map do |char|
-        encrypted_message = apply_shift(char)
-      end
-      encrypted_message
+def apply_shift(char_array)
+  char = char_array[0]
+  if char_array[1] % 4 == 0
+    shift_alphabet(char, @entered_shift_hash[:a])
+  elsif char_array[1] % 4 == 1
+    shift_alphabet(char, @entered_shift_hash[:b])
+  elsif char_array[1] % 4 == 2
+    shift_alphabet(char, @entered_shift_hash[:c])
+  elsif char_array[1] %4 == 3
+    shift_alphabet(char, @entered_shift_hash[:d])
+  end
+end
+
+def shift_alphabet(entered_char, shift)
+  encrypted_chars = []
+  character_set.each do |char|
+    if entered_char == char
+      rotated = character_set.rotate(shift)
+     encrypted_chars << rotated[0]
+
     end
   end
+  encrypted_chars
 end
 
-def apply_shift(char)
-  if char[1] % 4 == 0
-    apply_shift(char, :a)
-  elsif char[1] % 4 == 1
-    apply_shift(char, :b)
-  elsif char[1] % 4 == 2
-    apply_shift(char, :c)
-  elsif char[1] %4 == 3
-    apply_shift(char, :d)
+def encrypt(key, offset, message)
+  self.enter_shift(key, offset)
+  encrypted_message = []
+  letter_and_index = chars_and_index(message)
+    letter_and_index.each do |char|
+      if !validate_characters(char[0])
+        "Invalid Entry, please only use letters and spaces."
+      else
+          encrypted_message << apply_shift(char)
+      end
+    end
+    encrypted_message.flatten.join
   end
-end
-
 
 end
