0da586a4c9e334bbf0d178d0e7c737ea5f391406
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-04-15 15:06:29 -0600

    Adds initial tests for decrypt class

--- test/decrypt_test.rb
+++ test/decrypt_test.rb
@@ -9,6 +9,16 @@ require './lib/decrypt'
 
 class DecryptTest < Minitest::Test
   def setup
-    decrypt = Decrypt.new()
+    @decrypt = Decrypt.new("38492", "0943", "sqkzz")
+  end
+
+  def test_decrypt_exists
+    assert_instance_of Decrypt, @decrypt
+  end
+
+  def test_it_has_attributes
+    assert_equal "38492", @decrypt.key
+    assert_equal "0943", @decrypt.offset
+    assert_equal "sqkzz", @decrypt.encryption
   end
 end
