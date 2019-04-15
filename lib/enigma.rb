5f77987a0624c922ea17d6f20f999c88eec93034
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-04-15 16:48:04 -0600

    Adds welcome method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,8 +3,21 @@ require_relative './encrypt'
 
 class Enigma
 include ShiftGenerator
-
   def initialize
 
   end
+
+  def welcome_message
+    system "clear"
+    p "Welcome to Enigma!" sleep(3.0)
+
+  end
+
+  def run_encrypt
+    p "Let's encrypt a message!" sleep(2.0)
+    p " Enter your message using only letters and spaces. Punctuation will return an error. "
+    message = gets.chomp
+  
+  end
+
 end
