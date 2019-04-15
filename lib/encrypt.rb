d235e317288c31166ab448976d86982474ef86ca
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-04-15 07:12:44 -0600

    Adds split characters method

---
+++ lib/encrypt.rb
@@ -0,0 +1,6 @@
+module Encrypt
+  def split_characters(message)
+    split_message = message.downcase.split(//)
+    split_message
+  end
+end 
--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,7 +1,9 @@
 require_relative './shiftgenerator'
+require_relative './encrypt'
 
 class Enigma
 include ShiftGenerator
+include Encrypt
   def initialize
 
   end
