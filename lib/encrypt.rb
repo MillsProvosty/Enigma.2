6efa44cd3ddc947f8cdeafb6eda6051f82ad9c03
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-04-15 07:14:05 -0600

    Adds validate characters method

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -3,4 +3,13 @@ module Encrypt
     split_message = message.downcase.split(//)
     split_message
   end
-end 
+
+  def validate_characters(message)
+    split_message = split_characters(message)
+    split_message.all? do |char|
+      character_set.include?(char)
+    end
+  end
+
+  
+end
