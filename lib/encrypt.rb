09ca926783a7860038cf7c8a2b61121c028d0d67
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-04-15 07:16:21 -0600

    Adds enter shift method

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -12,12 +12,23 @@ module Encrypt
   end
 
   def chars_and_index(message)
-  characters = message.downcase.chars
-  letter_and_index = characters.map.with_index do |char, index|
-    [char, index]
+    characters = message.downcase.chars
+    letter_and_index = characters.map.with_index do |char, index|
+      [char, index]
+    end
+    letter_and_index
   end
-  letter_and_index
-end
+
+  
+  def enter_shift(key, offset)
+    entered_shift_hash = {}
+    entered_shift_hash[:a] = (key[-5..-4].to_i + offset[0].to_i)
+    entered_shift_hash[:b] = (key[-4..-3].to_i + offset[1].to_i)
+    entered_shift_hash[:c] = (key[-3..-2].to_i + offset[2].to_i)
+    entered_shift_hash[:d] = (key[-2..-1].to_i + offset[3].to_i)
+    entered_shift_hash
+  end
+
 
 
 end
