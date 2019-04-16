8ff406277f57a83c4d1d34bf93d700c53e62c005
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-04-16 17:03:28 -0600

    Adds functioning decrypt class

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -3,9 +3,13 @@ require './lib/enigma'
 require './lib/code_it'
 require './lib/decode'
 
-ARGV == [read_file, write_file]
+read_file, write_file, key, date = ARGV
 
 messages = File.open(read_file, "r")
-encrypted = File.open(write_file, "w")
+decrypted = File.open(write_file, "w")
 
-decrypt_message = Enigma.new.decrypt(encrypt.read)
+decrypt_message = Enigma.new.decrypt(messages.read, key, date)
+decrypted.write(decrypt_message[:decryption])
+decrypted.close
+
+p "Created #{write_file} with the key #{(decrypt_message[:key])} and date #{(decrypt_message[:date])}"
